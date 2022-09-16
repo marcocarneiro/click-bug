797563975c6376677632ac63de9210e4036ca36c
marcocarneiro <marco.carneiro7@gmail.com>
2022-09-16 12:39:11 -0300

    Etapas iniciais do Game

--- game.js
+++ game.js
@@ -1,127 +1,66 @@
-/* --------------------------------------------- 
-            VARIÁVEIS E FUNÇÕES 
-------------------------------------------------*/
+/* 
+---------------------------------------------------
+    VARIÁVEIS E FUNÇÕES
+---------------------------------------------------
+ */
+
 //lista com os invasores
-const invasores = document.getElementsByClassName('invasor')
-//lista com os bonzinhos
-const bonzinhos = document.getElementsByClassName('bonzinho')
-//largura do quadro
-const larguraQuadro = document.getElementById('quadro').offsetWidth
-//pontuação
-var score = 0
-//posicionamento em X e Y dos diversos objetos
-var posX, posY
+let invasores = document.getElementsByClassName('invasor')
+//lista com os "bonzinhos"
+let bonzinhos = document.getElementsByClassName('bonzinho')
 
-var tmpRestante = 30;
+let score = 0
+let tempoRestante = 30
+
+let larguraQuadro = document.getElementById('quadro').offsetWidth
 
 
-//função para posicionar os elementos, recebe argumento
-// el que é um determinado elemento do HTML
-function posicElement(el)
-{
-    posX = Math.random()*1000
-    posY = Math.random()*400
-
+//Função para posicionar um elemento
+//recebe parâmetro el que informa o elemento
+const posicElement = (el) => {
+    let posX = Math.floor(Math.random()*1000)
+    let posY = Math.floor(Math.random()*400)
     el.style.position = 'absolute'
-    el.style.left = -posX + 'px'
-    el.style.top = posY + 'px'
+    el.style.left = -posX+'px'
+    el.style.top = posY+'px'
 }
 
+//Desloca os elementos na tela
+//recebe parâmetros elemento, velocidade, incremento
+const moveElemento = (el, veloc, inc)=> {
 
-//função que desloca os elementos constantemente
-//recebe elemento, velocidade deslocamento
-function moveElemento(el, veloc, inc)
-{   
-    //Adiciona ao elemento o atributo velocidade
-    el.setAttribute('velocidade', inc)
-    //setInterval executa constantemente
-    var tempo = setInterval(function(){
-        veloc += inc;
-        el.style.left = veloc + 'px'
-        //Se veloc for maior q o quadro OU
-        //elemento tiver a classe 'morto'
-        if(veloc > larguraQuadro || el.classList.contains('morto'))
-        {
+    //executa a cada x milissegundos
+    const anima = setInterval(() => {
+        veloc = veloc + inc
+        el.style.left = veloc +'px'
+        //verifica se elemento saiu do quadro e possui a
+        //classe emtela
+        //remove a classe e volta para uma posição inicial
+        if(veloc > larguraQuadro){
+            veloc = -1000
             posicElement(el)
-            el.classList.remove('morto')
-            veloc = -Math.random()*500;
-            //sorteia de 1 a 30
-            inc = Math.random()*29+1
-            el.setAttribute('velocidade', inc);
         }
-    }, 20)
-}
-
-//Ao clicar nos invasores, soma score
-//e reinicia o invasor
-function mataInvasor(el)
-{                       
-    score += 10
-    /* SE ATRIBUTO VELOCIDADE FOR MAIOR QUE 16
-    INCREMENTA O SCORE EM + 90 */
-    if(el.getAttribute('velocidade') > 16)
-    {
-        score += 90
-
-        var pt = document.getElementById('pts100')
-        pt.style.left = el.style.left;
-        pt.style.top = el.style.top;                
-        //Processa a função abaixo após
-        //400 centézimos de segundo (esconde a pontuação)
-        var tmp = setInterval(() => {
-            pt.style.left = -500+'px';
-            clearInterval(tmp)
-        }, 400);
-    }
-
-    document.getElementById('score').innerText = score
-    //acrescenta a classe 'morto'
-    el.classList.add('morto')
-}
-
-//Ao clicar nos bonzinhos, diminui score
-//e reinicia o bonzinho
-function mataBonzinho(el)
-{
-    score -= 50
-    document.getElementById('score').innerText = score
-    
+    }, 40);    
 }
 
 
-/* --------------------------------------------
-            EVENTOS E EXECUÇÕES AUTOMÁTICAS 
------------------------------------------------*/
-document.getElementById('score').innerText = score
 
-//aplica as funções nos invasores
-for(let inv of invasores)
-{
+
+
+
+
+
+
+
+
+/* 
+------------------------------------------------------
+        EVENTOS E EXECUÇÕES AUTOMÁTICAS 
+------------------------------------------------------
+ */
+
+for(inv of invasores){
     posicElement(inv)
     moveElemento(inv, Math.random()*10, Math.random()*19+1)
-    inv.addEventListener('mousedown', function(){
-        mataInvasor(this)
-    })
 }
 
-//aplica as funções nos bonzinhos
-for(let bom of bonzinhos)
-{
-    posicElement(bom)
-    moveElemento(bom, Math.random()*10, Math.random()*19+1)
-    bom.addEventListener('mousedown', function(){
-        mataBonzinho(this)
-    })
-}
-
-//Contagem regressiva
-setTimeout(function () {
-    alert('TEMPO ESGOTADO!! SUA PONTUAÇÃO FOI DE ' + score)
-    document.location.reload(true);
-}, 30000);
-
-var tempoRestante = setInterval(() => {
-    tmpRestante --
-    document.getElementById('temporest').innerText = tmpRestante    
-}, 1000);
-        
